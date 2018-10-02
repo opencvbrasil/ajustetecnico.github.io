2fdf35876d7fb2fb04d620e6689d9cd58bc04813
agostinhobritojr <agostinhobritojr@gmail.com>
2018-10-02 07:28:36 -0300

    draft

--- _site/images/plots/plot3d.cpp
+++ _site/images/plots/plot3d.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 int main(void){
+  for(int i=0; i<41; i++){
+	cout << i << ",";
+  }
+  cout << endl;
   for(float x=-1; x<1; x=x+0.05){
 	for(float y=-1; y<1; y=y+0.05){
-	  cout << (x*x+y*y)*sin(6*x)*cos(6*y) << ", ";
+	  cout << (x*x+y*y)*sin(6*x)*cos(6*y) << ",";
 	}
 	cout << endl;
   }	
